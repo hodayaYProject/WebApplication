64a232615771bc9ce10288368429841bb31d2bfa
hodayaYProject <44978726+hodayaYProject@users.noreply.github.com>
2018-12-05 12:47:38 +0200

    Inital commit

---
+++ WebApplication/Controllers/PermissionsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class PermissionsController : ApiController
+    {
+        private FilesSearchDBEntities db = new FilesSearchDBEntities();
+
+        // GET: api/Permissions
+        public IQueryable<Permission> GetPermissions()
+        {
+            return db.Permissions;
+        }
+
+        // GET: api/Permissions/5
+        [ResponseType(typeof(Permission))]
+        public IHttpActionResult GetPermission(int id)
+        {
+            Permission permission = db.Permissions.Find(id);
+            if (permission == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(permission);
+        }
+
+        // PUT: api/Permissions/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPermission(int id, Permission permission)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != permission.KodP)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(permission).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PermissionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Permissions
+        [ResponseType(typeof(Permission))]
+        public IHttpActionResult PostPermission(Permission permission)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Permissions.Add(permission);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PermissionExists(permission.KodP))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = permission.KodP }, permission);
+        }
+
+        // DELETE: api/Permissions/5
+        [ResponseType(typeof(Permission))]
+        public IHttpActionResult DeletePermission(int id)
+        {
+            Permission permission = db.Permissions.Find(id);
+            if (permission == null)
+            {
+                return NotFound();
+            }
+
+            db.Permissions.Remove(permission);
+            db.SaveChanges();
+
+            return Ok(permission);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PermissionExists(int id)
+        {
+            return db.Permissions.Count(e => e.KodP == id) > 0;
+        }
+    }
+}
